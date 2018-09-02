0ca6fdc13ebf54b5c3bac7b01c0b64f455bfd1f1
Vivi-Iam <vivian.opondoh@gmail.com>
2018-09-02 05:34:07 +0300

    *Delete unused photos **Add parallax effect

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
-var myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world!';
+// var myHeading = document.querySelector('h1');
+// myHeading.textContent = 'Hello world!';
 
 // document.querySelector('html').onclick = function() {
 //     alert('Ouch! Stop poking me!');
@@ -7,12 +7,12 @@ myHeading.textContent = 'Hello world!';
 
 var myImage = document.querySelector('img');
 
-myImage.onmouseover = function() {
+myImage.onclick = function() {
     var mySrc = myImage.getAttribute('src');
-    if(mySrc === 'images/SAM_0235.JPG') {
-      myImage.setAttribute ('src','images/SAM_0234.JPG');
+    if(mySrc === 'images/iceskating.jpg') {
+      myImage.setAttribute ('src','images/car.jpg');
     } else {
-      myImage.setAttribute ('src','images/SAM_0235.JPG');
+      myImage.setAttribute ('src','images/swim.jpg');
     }
 }
 
