1dbaacf17aee977e999ccfd1b179805ed35111e5
Vivi-Iam <vivian.opondoh@gmail.com>
2018-09-03 06:25:01 +0300

    Add onclick to change them button *Set theme for whole site to change onbutton click **Add id since query by id

--- scripts/main.js
+++ scripts/main.js
@@ -12,22 +12,22 @@
 // BUTTONS
 
 var button = document.getElementById('btnhome');
-button.onclick = function() {
+button.onclick = function () {
   document.getElementById('home').scrollIntoView()
 }
 
 var button = document.getElementById('btnabout');
-button.onclick = function() {
+button.onclick = function () {
   document.getElementById('about').scrollIntoView()
 }
 
 var button = document.getElementById('btninterests');
-button.onclick = function() {
+button.onclick = function () {
   document.getElementById('interests').scrollIntoView()
 }
 
 var button = document.getElementById('btncontact');
-button.onclick = function() {
+button.onclick = function () {
   document.getElementById('contact').scrollIntoView()
 }
 
@@ -42,11 +42,36 @@ button.onclick = function() {
 //   }
 // }
 
-var button = document.getElementById('mentorship');
-button.onmouseover = function() {
+var element = document.getElementById('mentorship');
+element.onmouseover = function () {
   document.getElementById('mentorship').style.display = "none";
   document.getElementById('sport').style.display = "unset";
 }
+
+var changeTheme = document.getElementById('btntheme');
+changeTheme.onclick = function () {
+  document.getElementById('html').style.backgroundColor = "#23B6B9";
+  document.getElementById('home').style.backgroundColor = "#ED9A8C";
+  document.getElementById('buttons').style.backgroundColor = "inherit";
+  document.getElementById('btnhome').style.backgroundColor = "#23B6B9";
+  document.getElementById('btnhome').style.borderColor = "#23B6B9";
+  document.getElementById('btnabout').style.backgroundColor = "#23B6B9";
+  document.getElementById('btnabout').style.borderColor = "#23B6B9";
+  document.getElementById('btninterests').style.backgroundColor = "#23B6B9";
+  document.getElementById('btninterests').style.borderColor = "#23B6B9";
+  document.getElementById('btncontact').style.backgroundColor = "#23B6B9";
+  document.getElementById('btncontact').style.borderColor = "#23B6B9";
+  document.getElementById('btntheme').style.backgroundColor = "#23B6B9";
+  document.getElementById('btntheme').style.borderColor = "#23B6B9";
+  document.getElementById('contact').style.backgroundColor = "#ED9A8C";
+  document.getElementById('i').style.backgroundColor = "#23B6B9";
+  document.getElementById('j').style.backgroundColor = "#23B6B9";
+  document.getElementById('about-bg-diagonal').style.borderLeftColor = "#23B6B9";
+  document.getElementById('interest-bg-diagonal').style.borderRightColor = "#23B6B9";
+
+}
+
+
 
 // var myHeading = document.querySelector('h3');
 // myHeading.onmouseover = function() {
@@ -56,13 +81,13 @@ button.onmouseover = function() {
 
 var myImage = document.querySelector('#imgvvn');
 
-myImage.ondblclick = function() {
-    var mySrc = myImage.getAttribute('src');
-    if(mySrc === 'images/vivian.jpg') {
-      myImage.setAttribute ('src','images/vvn-mest.jpg');
-    } else {
-      myImage.setAttribute ('src','images/vivian.jpg');
-    }
+myImage.ondblclick = function () {
+  var mySrc = myImage.getAttribute('src');
+  if (mySrc === 'images/vivian.jpg') {
+    myImage.setAttribute('src', 'images/vvn-mest.jpg');
+  } else {
+    myImage.setAttribute('src', 'images/vivian.jpg');
+  }
 }
 
 // var myButton = document.querySelector('button');
@@ -84,5 +109,3 @@ myImage.ondblclick = function() {
 //   myButton.onclick = function() {
 //     setUserName();
 //   }
-  
-  
